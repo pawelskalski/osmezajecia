73ce3a1e96e4c1a3396a6697e88fa21c8e0d81d3
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2020-01-23 10:28:16 +0100

    End of work

---
+++ zajecia8/OutOfInkEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace zajecia8
+{
+    public class OutOfInkEventArgs : EventArgs
+    {
+        public OutOfInkEventArgs(string color)
+        {
+            this.color = color;
+        }
+        public string color { get; set; }
+    }
+}
---
+++ zajecia8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zajecia8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Printer();
+            printer.OutOfPaperEvent += OutOfPaperEventHandler;
+            printer.Print();
+        }
+
+        static void OutOfPaperEventHandler(object sender, EventArgs args)
+        {
+            Console.WriteLine("Printer ran out of paper, please put new paper");
+        }
+    }
+}
